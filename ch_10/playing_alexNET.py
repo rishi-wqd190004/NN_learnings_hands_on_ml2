5de6e0c9b3ba7550831997eb4d8b0f24fd5c3807
wqd190004-rishi <wqd190004@siswa.um.edu.my>
2021-09-12 02:47:07 +0800

    adding alexnet architecture

--- ch_10/playing_alexNET.py
+++ ch_10/playing_alexNET.py
@@ -56,5 +56,25 @@ valid_ds = (train_ds
                     .batch(batch_size=32, drop_remainder=True))
 
 # model implementation
-model = keras.
-                    
+model = keras.models.Sequential([
+    keras.layers.Conv2D(filters=96, kernel_size=(11,11), strides=(4,4), activation='relu', input_shape=(227,227,3)),
+    keras.layers.BatchNormalization(),
+    keras.layers.MaxPooling2D(pool_size=(3,3), strides=(2,2)),
+    keras.layers.Conv2D(filters=256, kernel_size=(5,5), strides=(1,1), activation='relu', padding='same'),
+    keras.layers.BatchNormalization(),
+    keras.layers.MaxPooling2D(pool_size=(3,3), strides=(2,2)),
+    keras.layers.Conv2D(filters=348, kernel_size=(3,3), strides=(1,1), activation='relu', padding='same'),
+    keras.layers.BatchNormalization(),
+    keras.layers.Conv2D(filters=348, kernel_size=(3,3), strides=(1,1), activation='relu', padding='same'),
+    keras.layers.BatchNormalization(),
+    keras.layers.Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), activation='relu', padding='same'),
+    keras.layers.BatchNormalization(),
+    keras.layers.MaxPooling2D(pool_size=(3,3), strides=(2,2)),
+    keras.layers.Flatten(),
+    keras.layers.Dense(4096, activation='relu'),
+    keras.layers.Dropout(0.5),
+    keras.layers.Dense(4096, activation='relu'),
+    keras.layers.Dropout(0.5),
+    keras.layers.Dense(10, activation='softmax')
+])
+                 
